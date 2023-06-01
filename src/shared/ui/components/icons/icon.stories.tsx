0752ba272268e9fa2885dda537c901bcf90ba405
Garrett Myrick <garrettmyrick@gmail.com>
2023-05-31 21:52:20 -0700

    updated Icon stories

--- src/shared/ui/components/icons/icon.stories.tsx
+++ src/shared/ui/components/icons/icon.stories.tsx
@@ -1,6 +1,13 @@
 import type { Meta, StoryObj } from "storybook-solidjs";
 import { Icon } from "./icon";
 
+const LabSVG = () => (
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    d="M9.75 3.104v5.714a2.25 2.25 0 01-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 014.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0112 15a9.065 9.065 0 00-6.23-.693L5 14.5m14.8.8l1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0112 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5"
+  />
+);
 
 const meta = {
   title: "Shared/UI/Components/Icon",
@@ -20,13 +27,8 @@ const meta = {
   },
   args: {
     size: 'md',
-    children: (
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M9.75 3.104v5.714a2.25 2.25 0 01-.659 1.591L5 14.5M9.75 3.104c-.251.023-.501.05-.75.082m.75-.082a24.301 24.301 0 014.5 0m0 0v5.714c0 .597.237 1.17.659 1.591L19.8 15.3M14.25 3.104c.251.023.501.05.75.082M19.8 15.3l-1.57.393A9.065 9.065 0 0112 15a9.065 9.065 0 00-6.23-.693L5 14.5m14.8.8l1.402 1.402c1.232 1.232.65 3.318-1.067 3.611A48.309 48.309 0 0112 21c-2.773 0-5.491-.235-8.135-.687-1.718-.293-2.3-2.379-1.067-3.61L5 14.5"
-      />
-    )
+    // @ts-ignore
+    children: () => <LabSVG />
   },
   parameters: {
     docs: {
@@ -47,9 +49,7 @@ export const Primary: Story = {
 export const Sizes: Story = {
   args: {
     class: 'border',
-    children: () => (
-      <path d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-    )
+    children: () => <LabSVG />
   },
   render: (props) => (
     <div class="flex flex-wrap gap-4">
