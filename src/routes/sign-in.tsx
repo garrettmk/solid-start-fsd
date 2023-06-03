283748137fd18d28b79441c2725c1c29f0b09514
Garrett Myrick <garrettmyrick@gmail.com>
2023-06-02 21:52:53 -0700

    updated sign-in route

--- src/routes/sign-in.tsx
+++ src/routes/sign-in.tsx
@@ -1,17 +1,27 @@
 import { useSession } from "@/entities/session";
+import { SignInWithPasswordForm, useSignInWithPassword } from "@/features/session/sign-in-with-password";
 import { AppURLDependency } from "@/shared/lib";
-import { Redirect, useContainer } from "@/shared/ui";
-import { SignInOverlay } from "@/widgets/session";
+import { Heading, Panel, Redirect, useContainer } from "@/shared/ui";
+import { Show } from "solid-js";
 
 export function SignIn() {
   const appURL = useContainer(AppURLDependency);
   const session = useSession();
   const isSignedIn = () => !!session();
+  const [signInResult, signInWithPassword] = useSignInWithPassword();
 
   return (
-    <main>
-      <Redirect to={appURL} when={isSignedIn} />
-      <SignInOverlay />
+    <main class="fixed inset-0 bg-gradient-to-r from-blue-700  via-blue-500 to-sky-500 animate-gradient bg-[size:400%] flex items-center justify-center">
+      <Redirect to={appURL} when={isSignedIn()} />
+      <Panel class="p-6 w-96 min-w-lg shadow-2xl">
+        <Heading class="text-2xl mb-4">Sign in to our platform</Heading>
+        <SignInWithPasswordForm onSubmit={signInWithPassword} />
+        <Show when={signInResult.error}>
+          <p class="mt-3 text-xs text-red-600 dark:text-red-400">
+            {signInResult.error?.message}
+          </p>
+        </Show>
+      </Panel>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        gradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        }
+      },
+      animation: {
+        gradient: 'gradient 10s ease infinite',
+      }
+    },
   },
   plugins: [],
   darkMode: 'class'
