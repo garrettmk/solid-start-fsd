8bc8df55914b29a085272ef79453a6639b11b65f
Garrett Myrick <garrettmyrick@gmail.com>
2023-06-04 08:59:02 -0700

    added stories for Steps

---
+++ src/shared/ui/components/steps/steps.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from "storybook-solidjs";
+import { Steps, Step } from "./steps";
+import { For } from "solid-js";
+import { IndexProvider } from "../../contexts";
+import { Button, ButtonNext, ButtonPrev } from "../buttons";
+import { HStack } from "../stacks";
+
+const meta = {
+  title: "Shared/UI/Components/Steps",
+  component: Steps,
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof Steps>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+  render: props => (
+    <div class="inline-block">
+      <IndexProvider initial={{ max: 3 }}>
+        <Steps {...props}>
+          <Step index={0}>Step 1</Step>
+          <Step index={1}>Step 2</Step>
+          <Step index={2}>Step 3</Step>
+        </Steps>
+        <HStack justify="around" class="mt-3">
+          <ButtonPrev>
+            Prev
+          </ButtonPrev>
+          <ButtonNext>
+            Next
+          </ButtonNext>
+        </HStack>
+      </IndexProvider>
+    </div>
+  )
+};
