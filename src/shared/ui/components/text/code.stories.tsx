2e09b9a42aaa61aa11a35f109a8c420ee7c0e949
Garrett Myrick <garrettmyrick@gmail.com>
2023-06-04 09:37:37 -0700

    added stories for Code

---
+++ src/shared/ui/components/text/code.stories.tsx
@@ -0,0 +1,33 @@
+import type { StoryObj } from "storybook-solidjs";
+import { Code } from './code';
+
+const meta = {
+  title: "Shared/UI/Components/Code",
+  component: Code,
+  tags: ["autodocs"],
+  argTypes: {
+    children: {
+      description: 'The code to display',
+      control: 'text',
+    }
+  },
+  args: {
+    children: JSON.stringify({
+      "name": "John Doe",
+      "age": 30,
+      "cars": [
+        {
+          "name": "Ford",
+          "models": ["Fiesta", "Focus", "Mustang"]
+        },
+      ]
+    }, null, 2),
+  }
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {},
+};
