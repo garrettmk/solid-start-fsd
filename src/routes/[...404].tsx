ed9ec0e0e75cd0c73c48844c6b906e8263d4d6c4
Garrett Myrick <garrettmyrick@gmail.com>
2023-08-15 08:28:21 -0700

    updated 404 page

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,19 +1,22 @@
-import { Title } from "solid-start";
-import { HttpStatusCode } from "solid-start/server";
+import { Heading } from "@/shared/ui";
+import { PageContent, PageHeader } from "@/widgets/page";
 
 export default function NotFound() {
   return (
-    <main>
-      <Title>Not Found</Title>
-      <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
-      <p>
+    <>
+      <PageHeader>
+        <Heading class="text-lg font-medium">Not Found</Heading>
+      </PageHeader>
+      <PageContent>
+        <Heading class="text-md mb-6">Page Not Found</Heading>
+        <p>
         Visit{" "}
         <a href="https://start.solidjs.com" target="_blank">
           start.solidjs.com
         </a>{" "}
         to learn how to build SolidStart apps.
       </p>
-    </main>
+      </PageContent>
+    </>
   );
 }
